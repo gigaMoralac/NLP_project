5599d9febfa70d30b2545eb5d2418d5660345310
gigaMoralac <vojindedovic@gmail.com>
2022-07-23 21:49:48 +0200

    models

--- training/trainer.py
+++ training/trainer.py
@@ -1,12 +1,18 @@
 """Aspect and sentiment analysis training."""
 import argparse
-from typing import List
+from typing import List, Optional
 from pathlib import Path
 
+from tqdm import tqdm
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.pipeline import Pipeline
 from training.dataset import Dataset
 from training.transforms import (StemSerbian, Transform, RemovePunctuation, CyrToLat, RemoveStopWords)
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from training.dataset import COLUMNS
 
 
 class Trainer():
@@ -24,11 +30,15 @@ class Trainer():
 
         self._transformer_choice = {"stem": StemSerbian, "remove_punct": RemovePunctuation, 
                                     "cyr_to_lat": CyrToLat, "stop_words": RemoveStopWords}
+        
+                
+        self._model_choice = {"logistic_regression": LogisticRegression, "naive_bayes": MultinomialNB, "svm": SVC }
         self._select_embedder()
         self._load_dataset(args.data_paths, args.exclude_value)
         self._select_transformer()
         self.transform_dataset()
         self._fit_embedder()
+        self._select_model()
 
     def _load_dataset(self, data_paths: List[Path], exclude_value: str) -> None:
         """Load dataset.
@@ -56,7 +66,7 @@ class Trainer():
     def _fit_embedder(self) -> None:
         """Fit embedder with assigned dataset.""" # TODO: fitting should be performed only on training set
         text_np, _ = self.dataset.to_numpy()
-        self.features = self._embedder.fit_transform(text_np)
+        self._features = self._embedder.fit_transform(text_np)
 
     def _select_transformer(self) -> None:
         """Select transformer or combinations of transformers based on commmand line arguments."""
@@ -67,4 +77,30 @@ class Trainer():
         """Apply assigned transformations to dataset."""
         transformed_dataset = self._transformer(self.dataset)
         self._dataset = transformed_dataset
+    
+    def _select_model(self):
+        self._model_dict = {column: self._model_choice[self._args.model]() for column in COLUMNS}
+    
+    def fit(self) -> None:
+        _, labels_np = self.dataset.to_numpy()
+        for i, column in tqdm(enumerate(COLUMNS)):
+            labels_column = labels_np[:, i]
+            labels_column = (labels_column != "None").astype(int)
+            self._model_dict[column].fit(self._features, labels_column)
+            print(f"Model trained for class: {column}")
+    
+    def predict(self, features:np.ndarray) -> np.ndarray:
+        predictions = []
+        for column in COLUMNS:
+            pred = self._model_dict[column].predict(features)
+            predictions.append(pred)
+        
+        predictions = np.array(predictions).T
+
+        return predictions
+            
+
+
+
+
 
