775897371791e9951c0baa7503ea0aa8e0506258
gigaMoralac <vojindedovic@gmail.com>
2022-09-18 10:02:10 +0200

    changes f1 averaging to macro

--- training/trainers.py
+++ training/trainers.py
@@ -12,7 +12,7 @@ class AspectTrainer(AbstractTrainer):
             args (argparse.Namespace): command-line arguments.
         """
         self._mapping_to_labels = {"None": 0., "Positive": 1., "Negative": 1., "Neutral": 1.}
-        self._f1_averaging = "binary"
+        self._f1_averaging = "macro"
         super().__init__(args)
 
 class SentimentTrainer(AbstractTrainer):
@@ -24,7 +24,7 @@ class SentimentTrainer(AbstractTrainer):
             args (argparse.Namespace): command-line arguments.
         """
         self._mapping_to_labels = {"None": None, "Positive": 0., "Negative": 1., "Neutral": 2.}
-        self._f1_averaging = "weighted"
+        self._f1_averaging = "macro"
         super().__init__(args)
 
 class JointTrainer(AbstractTrainer):
@@ -36,5 +36,5 @@ class JointTrainer(AbstractTrainer):
             args (argparse.Namespace): command-line arguments.
         """
         self._mapping_to_labels = {"None": 0., "Positive": 1., "Negative": 2., "Neutral": 3.}
-        self._f1_averaging = "weighted"
+        self._f1_averaging = "macro"
         super().__init__(args)
